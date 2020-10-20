528454dd72cd4b998dca0e55366896e789829018
Oktado Putra <oktado.putra@gmail.com>
2020-10-20 22:51:12 +0700

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+//Exercise 1
+function selamat() {
+    var pembeli = prompt('Masukkan nama anda :')
+    var barang = prompt('Masukkan barang yang akan anda beli.')
+    return 'Terima Kasih '+pembeli+' karena telah membeli produk '+ barang +' di toko kami.'   
+}
+    alert(selamat())
